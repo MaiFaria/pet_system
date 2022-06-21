b857104459303a697a758c6c191f8612f0c7a077
Maiara Faria <maiara.santos@theos.com.br>
2022-06-21 20:41:52 -0300

    Iniciando API de Identidade com implementação de Testes Unitários

---
+++ src/Services/PS.Identity/Validations/UserRegisterValidation.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using static PS.Identity.Models.UserViewModels;
+using static PS.Core.Helpers.ResourceString;
+
+namespace PS.Identity.Validations
+{
+    public class UserRegisterValidation : AbstractValidator<UserRegister>
+    {
+        public UserRegisterValidation()
+        {
+            RuleFor(row => row.Name)
+                .NotEmpty()
+                .WithMessage(Mensagens.MSG_NOME_OBRIGATORIO);
+
+            RuleFor(row => row.Cpf)
+                .NotEmpty()
+                .WithMessage(Mensagens.MSG_CPF_OBRIGATORIO);
+
+            RuleFor(row => row.Email)
+                .NotEmpty()
+                .WithMessage(Mensagens.MSG_EMAIL_OBRIGATORIO);
+
+            RuleFor(row => row.Password)
+                .NotEmpty()
+                .Length(6, 10)
+                .WithMessage(string.Format(Mensagens.MSG_GEN_TAMANHO_CAMPO, Campos.PASSWORD, 6, 10));
+
+            RuleFor(row => row)
+                .Custom((row, contexto) =>
+                {
+                    if (row.PasswordConfirmation != row.Password)
+                        contexto.AddFailure(Mensagens.MSG_SENHA_NAO_CONFERE);
+                });
+        }
+    }
+}
