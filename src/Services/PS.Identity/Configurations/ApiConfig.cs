d1f01e3970538fa0592e5c7898d3c49556c04494
Maiara Faria <maiara.santos@theos.com.br>
2022-06-23 08:24:47 -0300

    Implementação de Controller para API de Identidade

---
+++ src/Services/PS.Identity/Configurations/ApiConfig.cs
@@ -0,0 +1,35 @@
+﻿namespace PS.Identity.Configurations
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityConfiguration();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/Services/PS.Identity/Extensions/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace PS.Identity.Extensions
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
